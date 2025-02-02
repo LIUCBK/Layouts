e092d98cd606cbe44231c5ff6e13b8dfc58775d6
LIUCBK <liucbk@bc.edu>
2025-02-02 13:18:10 -0500

    Initial Commit

---
+++ Layouts/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Layouts
+//
+//  Created by 刘佳雨 on 2025/2/2.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Layouts/LayoutsApp.swift
@@ -0,0 +1,17 @@
+//
+//  LayoutsApp.swift
+//  Layouts
+//
+//  Created by 刘佳雨 on 2025/2/2.
+//
+
+import SwiftUI
+
+@main
+struct LayoutsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
